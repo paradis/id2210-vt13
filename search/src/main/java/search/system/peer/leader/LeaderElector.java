ed4c71e37c1bd89a2e106a4b6cd13e2d2484b22f
Thomas Paradis <paradis@kth.se>
2013-05-19 14:41:56 +0200

    Add comments for Leader Elector (research part)

--- search/src/main/java/search/system/peer/leader/LeaderElector.java
+++ search/src/main/java/search/system/peer/leader/LeaderElector.java
@@ -4,6 +4,7 @@
  */
 package search.system.peer.leader;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,10 @@ import search.system.peer.leader.LeaderMsg.Reject;
 import search.system.peer.search.SearchInit;
 import tman.system.peer.tman.TManSample;
 
+/**
+ * This class is responsible for electing a leader and return a functional leader.
+ * It will maintain a list of peers close to the leader and contact them when performing a search of a leader.
+ */
 public class LeaderElector extends ComponentDefinition{
     private static final Logger logger = LoggerFactory.getLogger(LeaderElector.class);
 
@@ -45,14 +50,27 @@ public class LeaderElector extends ComponentDefinition{
     Positive<Timer> timerPort = positive(Timer.class);
 
     Address self;
+    
+    ///
+    // Leader research
+    ///
+    
     List<Address> tmanNeighbours = null;
-    ConcurrentSkipListSet<Address> bestPeers;    // Known peers with highest utility
+    // Known peers with highest utility
+    ConcurrentSkipListSet<Address> bestPeers;
+    // Current requests for leader infos
     Map<Address, UUID> currentRequests;
 
+    // Current living leader
     Address currentLeader = null;
+    // The old leader which is believed to be offline to notify other nodes and avoid that they give it back to us.
     Address oldLeader = null;
+    // If our peer is waiting to know who the leader is (to differentiate it from an automatic task)
     boolean peerRequest = false;
 
+    ///
+    // Leader election
+    /// 
     List<Address> expectedElectors = null;  // List of electors whose answer is awaited ; must be null if no ongoing election
 
     public LeaderElector() {
@@ -81,12 +99,23 @@ public class LeaderElector extends ComponentDefinition{
         public void handle(SearchInit init) {
             self = init.getSelf();
 
+            ///
+            // Regularly, we will try to find out who the leader to accelerate convergence in a request by our peer.
+            // To ensure that all nodes make their request at the same time, the launch is shifted randomly.
+            ///
             SchedulePeriodicTimeout rst = new SchedulePeriodicTimeout(new Random().nextInt(config_timeout_update), config_timeout_update);
             rst.setTimeoutEvent(new UpdateLeaderTimeout(rst));
             trigger(rst, timerPort);
         }
     };
 
+//------------------------------------------------------------------------------
+//                 Leader Research
+//------------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // Lauch a leader request
+    
     /*
      * The peer ask for the leader
      */
@@ -99,26 +128,63 @@ public class LeaderElector extends ComponentDefinition{
             
             peerRequest = true;
 
-            //already done
+            // we are the leader, nothing else to do
             if (currentLeader == self)
             {
                 peerRequest = false;
                 trigger(new LeaderResponse(self), leaderElectionPort);
                 logger.debug(self.getId()+" : LeaderRequest: We are the leader");
             }
-            //here we check if the leader is up
+            // here we check if the leader is up
             else if (currentLeader != null) {
                 askInfos(currentLeader);
             }
+            // we have to search for the leader
             else {
                 launchRequests();
             }
 
         }
     };
-
+    
     /*
-     * another peer's LeaderElector wants to know our leader and best peers
+     * Request for looking for the leader.
+     * It can be automatic or the result of an old inability to continue our search for lack of pee
+     */
+    Handler<UpdateLeaderTimeout> handleUpdateLeaderTimeout = new Handler<UpdateLeaderTimeout>() {
+        @Override
+        public void handle(UpdateLeaderTimeout e) {
+            // If we have a leader, we check if it is online before launching requests
+            if (currentLeader != null)
+                askInfos(currentLeader);
+            else
+                launchRequests();
+        }
+    };
+    
+    //--------------------------------------------------------------------------
+    // Ask info from another peer and manage response or timeout
+    
+    /*
+     * Ask for leader and best peers to a peer and launch a timeout in case of the peer is offline
+     */
+    void askInfos(Address peer)
+    {
+        // A request is currently pending
+        if (currentRequests.containsKey(peer)) {
+            return;
+        }
+
+        trigger(new LeaderMsg.AskLeaderInfos(self, peer, oldLeader), networkPort);
+
+        ScheduleTimeout st = new ScheduleTimeout(config_timeout_info);
+        st.setTimeoutEvent(new LeaderInfosTimeout(st));
+        currentRequests.put(peer, st.getTimeoutEvent().getTimeoutId());
+        trigger(st, timerPort);
+    }
+    
+    /*
+     * Another peer's LeaderElector wants to know our leader and best peers
      */
     Handler<LeaderMsg.AskLeaderInfos> handleAskLeaderInfos = new Handler<LeaderMsg.AskLeaderInfos>() {
         @Override
@@ -126,7 +192,9 @@ public class LeaderElector extends ComponentDefinition{
             if (e.getSuspectedLeader() != null && e.getSuspectedLeader() == currentLeader)
             {
                 logger.debug(self.getId()+" : Leader (" + currentLeader.getId() + ") suspected by " + e.getSource().getId());
+                // The peer has suspected our leader, we check if it is true
                 askInfos(currentLeader);
+                // And we avoi to response with a suspected leader
                 trigger(new LeaderMsg.AnswerLeaderInfos(self, e.getSource(), null, bestPeers), networkPort);
             }
             else {
@@ -136,14 +204,16 @@ public class LeaderElector extends ComponentDefinition{
     };
 
     /*
-     * we receive a leader and a list of best peers
+     * We receive a leader and a list of best peers.
      */
     Handler<LeaderMsg.AnswerLeaderInfos> handleAnswerLeaderInfos = new Handler<LeaderMsg.AnswerLeaderInfos>() {
         @Override
         public void handle(AnswerLeaderInfos e) {
+            // We cancel the timeout associated with this request
             trigger (new CancelTimeout(currentRequests.get(e.getSource())), timerPort);
             currentRequests.remove(e.getSource());
 
+            // We merge our best peers
             bestPeers.addAll(e.getBestPeers());
             cleanBestPeers();
 
@@ -162,38 +232,50 @@ public class LeaderElector extends ComponentDefinition{
                 }
                 else
                 {
+                    // Our leader no longer considers itself as the leader anymore
                     if (currentLeader == e.getSource()) {
                         currentLeader = null;
                     }
 
+                    // And we contact the supposed new leader
                     bestPeers.add(e.getCurrentLeader());
                     askInfos(e.getCurrentLeader());
                 }
             }
             else {
+                // No more information on the leader, we try to launch requests with potential new peers that we obtained
                 launchRequests();
             }
 
         }
     };
 
+    /*
+     * A request for information has not been successful, we assume that the peer is offline and we remove from our lists.
+     */
     Handler<LeaderInfosTimeout> handleLeaderInfosTimeout = new Handler<LeaderInfosTimeout>() {
         @Override
         public void handle(LeaderInfosTimeout e) {
 
+            // retrieve the timeout associated with the request 
             Address address = getKeyFromValue(e.getTimeoutId());
 
-            if (address != null)
+            if (address == null)
+                logger.error(self.getId() + " handleLeaderInfosTimeout: unknown request!");
+            else
             {
                 currentRequests.remove(address);
 
+                // The leader is down
                 if (currentLeader == address)
                 {
                     logger.debug(self.getId()+" : No answers from leader: " + currentLeader.getId());
+                    // To inform the other peers and avoid useless response with this leader
                     oldLeader = currentLeader;
                     currentLeader = null;
                 }
 
+                // This peer should not be considered anymore
                 bestPeers.remove(address);
             }
 
@@ -204,22 +286,26 @@ public class LeaderElector extends ComponentDefinition{
         }
     };
 
-
+        
+    //--------------------------------------------------------------------------
+    // Launch multiple requests since our leader is not responding anymore
+    
     /*
-     * Lauch a request for the leader and some best peers to the best peers we know
+     * Lauch requests for the leader and best peers to the best peers we know
      */
     void launchRequests()
     {
+        // If the leader is still unknown and we have not exceeded our limit of concurrent requests.
         while(currentLeader == null && currentRequests.size() <= config_max_requests)
         {
+            // Select a peer which may be contacted
             Address peer = selectNextPeer();
 
-            if (peer != null) {
+            if (peer != null)
                 askInfos(peer);
-            }
             else
             {
-                // we have no possibility to find a leader, so we wait and try again
+                // we have no possibility to find a leader, so we wait some time and try again
                 if (currentRequests.isEmpty())
                 {
                     ScheduleTimeout rst = new ScheduleTimeout(config_delay_tryagain);
@@ -233,48 +319,21 @@ public class LeaderElector extends ComponentDefinition{
     }
 
     /*
-     * Select a peer which may be contacted
+     * Select a peer which may be contacted:
+     * We select the best peer we known which is not currently contacted
      */
     Address selectNextPeer() {
-        // TODO: à vérifier, en particulier itération
-        for (Address peer : bestPeers) {
-            if (!currentRequests.containsKey(peer)) {
+        for (Address peer : bestPeers)
+            if (!currentRequests.containsKey(peer))
                 return peer;
-            }
-        }
 
         return null;
     }
 
-    /*
-     * Ask for leader and peers to a peer
-     */
-    void askInfos(Address peer)
-    {
-        if (currentRequests.containsKey(peer)) {
-            return;
-        }
-
-        trigger(new LeaderMsg.AskLeaderInfos(self, peer, oldLeader), networkPort);
-
-        ScheduleTimeout st = new ScheduleTimeout(config_timeout_info);
-        st.setTimeoutEvent(new LeaderInfosTimeout(st));
-        currentRequests.put(peer, st.getTimeoutEvent().getTimeoutId());
-        trigger(st, timerPort);
-    }
-
-    Handler<UpdateLeaderTimeout> handleUpdateLeaderTimeout = new Handler<UpdateLeaderTimeout>() {
-        @Override
-        public void handle(UpdateLeaderTimeout e) {
-            if (currentLeader != null) {
-                askInfos(currentLeader);
-            }
-            else {
-                launchRequests();
-            }
-        }
-    };
-
+    
+    //--------------------------------------------------------------------------
+    // Refresh information with TMan 
+    
     Handler<TManSample> handleTManSample = new Handler<TManSample>() {
         @Override
         public void handle(TManSample event) {
@@ -307,7 +366,23 @@ public class LeaderElector extends ComponentDefinition{
             }
         }
     };
+    
+    /*
+     * Utility function: remove some best peers after merging other LeaderElector's best peers
+     */
+    void cleanBestPeers()
+    {
+        bestPeers.remove(self);
 
+        while(bestPeers.size() >= config_size_bestPeers) {
+            bestPeers.pollLast();
+        }
+    }
+    
+//------------------------------------------------------------------------------
+//                 Leader election
+//------------------------------------------------------------------------------
+   
     public void launchElection() {
         // Ask all tman neighbours if I can be the leader
         for (Address dest : tmanNeighbours) {
@@ -392,28 +467,24 @@ public class LeaderElector extends ComponentDefinition{
         }
     };
 
+    /*
+     * Return best peer (max Id) from a list of peer and another address
+     */
     Address getBestPeerFrom(List<Address> peers, Address other) {
-        if (peers == null || peers.isEmpty()) {
+        if (peers == null)
             return other;
-        }
-
-        Address best = peers.get(0);
-        for (Address a : peers) {
+        
+        ArrayList<Address> myPeers = new ArrayList<Address>(peers);
+        myPeers.add(other);
+        
+        Address best = myPeers.get(0);
+        for (Address a : myPeers) {
             if (a.getId() > best.getId()) {
                 best = a;
             }
         }
-
-        if (other == null) {
-            return best;
-        }
-
-        if (other.getId() > best.getId()) {
-            return other;
-        }
-        else {
-            return best;
-        }
+        
+        return best;
     }
 
     // For debug purposes
@@ -424,15 +495,6 @@ public class LeaderElector extends ComponentDefinition{
             str += d.getId()+"; ";
         }
         return str + "]";
-    }
-
-    void cleanBestPeers()
-    {
-        bestPeers.remove(self);
-
-        while(bestPeers.size() >= config_size_bestPeers) {
-            bestPeers.pollLast();
-        }
     }
 
     Address getKeyFromValue(UUID value) {
